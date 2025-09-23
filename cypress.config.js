c108f3c9a9fe5c773d7dac600155a1c4578102fc
Oksana <oksanarevakina1@gmail.com>
2025-09-23 16:52:12 +0200

    Cypress homework2

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,19 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    baseUrl: 'https://guest:welcome2qauto@qauto.forstudy.space',
+    retries: {
+      runMode: 1,
+      openMode: 1,
     },
+    viewportHeight: 1080,
+    viewportWidth: 1920,
+    defaultCommandTimeout: 8000,
+    pageLoadTimeout: 30000,
+    video: true,
+    screenshotOnRunFailure: true,
   },
+
+  projectId: "axofaa",
 });
+
